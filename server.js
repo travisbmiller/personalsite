482ea91e4f08fdc33d265907547fdb4e0d03ecaa
Travis Miller <travisbmiller@outlook.com>
2015-12-30 20:32:27 -0800

    added favicon

--- server.js
+++ server.js
@@ -1,9 +1,11 @@
 var express = require('express');
+var favicon = require('serve-favicon');
+
 var app = express();
 var port = 9000;
 
 app.use(express.static(__dirname + '/client'));
-
+app.use(favicon(__dirname + '/client/favicon/favicon.ico'));
 
 app.get("/", function (req, res) {
   console.log("req");
